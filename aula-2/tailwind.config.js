6fe9ab4769a2bb49e9b9b165b959baca52ff8278
Roberto Cestari <robertotcestari@gmail.com>
2021-06-14 06:58:34 -0300

    aula 2 🐈

---
+++ aula-2/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    extend: {
+
+      colors: {
+        'purple-solid': '#7159A4',
+        'purple-dark-solid': '#49317D',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
